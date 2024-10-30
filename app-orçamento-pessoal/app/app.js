17d32abe52961e407761563d9066c3a905f06948
diatsilva007 <diatsilva007@gmail.com>
2024-10-29 22:43:05 -0300

    feat: lógica da classe Despesa criada

--- app-orçamento-pessoal/app/app.js
+++ app-orçamento-pessoal/app/app.js
@@ -1,5 +1,16 @@
+class Despesa {
+   constructor(ano, mes, dia, tipo, descricao, valor) {
+      this.ano = ano;
+      this.mes = mes;
+      this.dia = dia;
+      this.tipo  = tipo;
+      this.descricao = descricao;
+      this.valor = valor;
+   }
+}
+
 function cadastrarDespesa() {
-    
+
    let ano = document.getElementById('ano')
    let mes = document.getElementById('mes')
    let dia = document.getElementById('dia')
@@ -7,5 +18,17 @@ function cadastrarDespesa() {
    let descricao = document.getElementById('descricao')
    let valor = document.getElementById('valor')
 
-   console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)   
-}
+   console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+
+   let despesa = new Despesa(
+      ano.value,
+      mes.value,
+      dia.value,
+      tipo.value,
+      descricao.value,
+      valor.value 
+   )
+
+   console.log(despesa)
+   
+}  
