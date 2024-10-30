62b512739951c842803486f5fdd95a42ecf35296
diatsilva007 <diatsilva007@gmail.com>
2024-10-30 17:42:25 -0300

    feat: Classe Bd e lógica para índices dinâmicos criadas e atualizadas

--- app-orçamento-pessoal/app/app.js
+++ app-orçamento-pessoal/app/app.js
@@ -1,38 +1,58 @@
 class Despesa {
-   constructor(ano, mes, dia, tipo, descricao, valor) {
-      this.ano = ano;
-      this.mes = mes;
-      this.dia = dia;
-      this.tipo  = tipo;
-      this.descricao = descricao;
-      this.valor = valor;
-   }
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
 }
+
+class Bd {
+
+	constructor() {
+		let id = localStorage.getItem('id')
+
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
+
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
+
+	gravar(d) {
+		let id = this.getProximoId()
+
+		localStorage.setItem(id, JSON.stringify(d))
+
+		localStorage.setItem('id', id)
+	}
+}
+
+let bd = new Bd()
+
 
 function cadastrarDespesa() {
 
-   let ano = document.getElementById('ano')
-   let mes = document.getElementById('mes')
-   let dia = document.getElementById('dia')
-   let tipo = document.getElementById('tipo')
-   let descricao = document.getElementById('descricao')
-   let valor = document.getElementById('valor')
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
 
-   console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
 
-   let despesa = new Despesa(
-      ano.value,
-      mes.value,
-      dia.value,
-      tipo.value,
-      descricao.value,
-      valor.value 
-   )
-
-   gravar(despesa)
-}  
-
-function gravar(d) {
-   // Simulação de gravação no localStorage
-   localStorage.setItem('despesa', JSON.stringify(d))
+	bd.gravar(despesa)
 }
