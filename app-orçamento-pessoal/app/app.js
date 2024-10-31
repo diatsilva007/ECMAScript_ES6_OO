63afdd5e8d795eda18c7e9b043f51c0a866e9c43
diatsilva007 <diatsilva007@gmail.com>
2024-10-31 20:20:01 -0300

    feat: lógica para validação de dados antes do registro concluída (modal: success/modal:error)

--- app-orçamento-pessoal/app/app.js
+++ app-orçamento-pessoal/app/app.js
@@ -9,8 +9,8 @@ class Despesa {
 	}
 
 	validarDados() {
-		for (let i in this) {
-			if (this[i] === undefined || this[i] == '' || this[i] == null) {
+		for(let i in this) {
+			if(this[i] == undefined || this[i] == '' || this[i] == null) {
 				return false
 			}
 		}
@@ -23,7 +23,7 @@ class Bd {
 	constructor() {
 		let id = localStorage.getItem('id')
 
-			if (id === null) {
+		if(id === null) {
 			localStorage.setItem('id', 0)
 		}
 	}
@@ -55,20 +55,35 @@ function cadastrarDespesa() {
 	let valor = document.getElementById('valor')
 
 	let despesa = new Despesa(
-		ano.value,
-		mes.value,
-		dia.value,
-		tipo.value,
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
 		descricao.value,
 		valor.value
 	)
 
-	if (despesa.validarDados()) { //true = valid ou false = invalid
-		bd.gravar(despesa)
+
+	if(despesa.validarDados()) {
+		//bd.gravar(despesa)
+
+		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+
 		//dialog de sucesso
-		$('#sucessoGravacao').modal('show')
+		$('#modalRegistraDespesa').modal('show') 
 	} else {
+		
+		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
 		//dialog de erro
-		$('#erroGravacao').modal('show')
+		$('#modalRegistraDespesa').modal('show') 
 	}
 }
